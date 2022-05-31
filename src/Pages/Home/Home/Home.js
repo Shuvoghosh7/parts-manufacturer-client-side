892871fdf1580d12de421463128ce25ef93b19b4
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-31 21:40:25 +0600

    change some contant

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <h1>Home Page</h1>
             <Banner/>
             <Tools/>
             <BusinessSummary/>

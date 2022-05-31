f44c6fd152e653440629dc11d23c1191b8da41e5
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-31 21:05:50 +0600

    fix some problem

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
             <Tools/>
             <BusinessSummary/>
             <MostOrderCountries/>
-            <UpcomingProducts/>
+            
             <Reviews/>
             
         </div>

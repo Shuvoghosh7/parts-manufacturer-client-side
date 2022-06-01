14d648abb8976c4e74e276b101c8009d58ea934e
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-06-02 02:57:48 +0600

    changing banner part

--- src/Pages/Home/Home/Banner.js
+++ src/Pages/Home/Home/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import img from '../../../images/Banner.png'
+import img from '../../../images/computer-parts-from-china.jpg'
+
 const Banner = () => {
     return (
-        <div>
-            <img className='w-full' src={img} alt="" />
+        <div class="hero  bg-base-100 lg:my-12">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={img} class=" w-[650px] rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold"> Manufacturer Products <br /> of Company!</h1>
+                    <p class="py-6">This company basically Manufacturer different parts of computers. These companies have been producing their products for a long time with their reputation. </p>
+                    
+                </div>
+            </div>
         </div>
     );
 };
